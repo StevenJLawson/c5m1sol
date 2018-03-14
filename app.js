a694e86b3190a2bd77d1f4a80820a639e3c509ac
Steven Lawson <sjlawson@gmail.com>
2018-03-14 13:36:12 -0700

    Appear to be done, bonus work not attempted. Boring.

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
+(function () {
+'use strict';
 angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.message = "";
-
+  $scope.input = ""
   $scope.checkAmount = function () {
-    console.log("BLAH");
+    var msg = checkFoodAmount($scope.input);
+    $scope.message = msg;
     return "Yaakov likes to eat healthy snacks at night!";
   };
-
 }
+function checkFoodAmount(str) {
+  if(str.length <= 0){
+    return "Please enter data first";
+  }
+  var arra = str.split([","]);
+  var msg;
+  console.log(arra.length);
+  console.log(arra);
+  if(arra.length > 3){
+    msg = "Too much!";
+  }else if(arra.length <= 0){
+    msg = "Please enter data first";
+  }else{
+    msg = "Enjoy!";
+  }
+  return msg;
+}
+
+
+})();
